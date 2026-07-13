c798dd151fe496b73737ac6338e8db9c8936ecf3
Komal <kokanekomal44@gmail.com>
2026-07-13 16:55:31 +0530

    Updated Base Test

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class BaseTest {
@@ -22,7 +23,7 @@ public class BaseTest {
 	
 	@BeforeMethod
 	@Parameters("browser")
-	public void setup(String browser) throws IOException
+	public void setup(@Optional("chrome")String browser) throws IOException
 	{
 	
 	p=new Properties();
